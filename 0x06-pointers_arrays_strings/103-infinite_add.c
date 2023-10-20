344ab2625fe8cc249deb9b31aeee6c7ea0b11c90
Ronald-tech <ronieqtsang@gmail.com>
2023-10-20 04:51:22 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -26,7 +26,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if (l + 1 > size_r)
 		return (0);
 	r[l] = '\0';
-	for (k = l - 1; k >= 0; k--)
+	for (k = l - 1 ; k >= 0 ; k--)
 	{
 		i--;
 		j--;
@@ -43,7 +43,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	}
 	if (d == 1)
 	{
-		r[l + 1] - '\0';
+		r[l + 1] = '\0';
 		if (l + 2 > size_r)
 			return (0);
 		while (l-- >= 0)
