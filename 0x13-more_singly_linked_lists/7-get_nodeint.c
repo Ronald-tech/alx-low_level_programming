c4579777d085e17d8e6fe5545cfc0b568c1dd63a
Ronald-tech <ronieqtsang@gmail.com>
2023-11-21 06:53:57 -0800

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,9 +7,9 @@
  *
  * Return: pointer to node
  */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *index;
+	listint_t *node;
 	unsigned int n;
 
 	for (node = head, n = 0; node && n < index; node = node->next, n++)
