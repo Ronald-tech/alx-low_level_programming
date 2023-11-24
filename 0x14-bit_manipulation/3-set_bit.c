c4fbd582d4065a8167c9e4e9ad165cd25666537f
Ronald-tech <ronieqtsang@gmail.com>
2023-11-24 09:04:10 -0800

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * set_bit - gets the bit at the index
+ * set_bit - sets the bit at the index
  * @n: the number to index
  * @index: the bit to get
  *
  * Return: 1 if successful, or -1 on error
  */
-int set_bit(unsigned long int n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(n) * 8)
 		return (-1);
