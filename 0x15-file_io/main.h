3fa25145446c1e47d322daadfb1f888c3e687d1d
Ronald-tech <ronieqtsang@gmail.com>
2023-11-27 11:19:00 -0800

    0-read_textfile.c

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <fcnt1.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define READ_BUF_SIZE 1024
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+
+#endif
