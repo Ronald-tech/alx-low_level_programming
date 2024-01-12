48c7a68b31614044f46b702e9a282b43d21a16ff
Ronald-tech <ronieqtsang@gmail.com>
2024-01-12 02:32:45 -0800

    ‘7-insert_dnodeint.c’

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,7 +32,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	new->n = n;
-	new-prev = tmp;
+	new->prev = tmp;
 	new->next = tmp->next;
 	tmp->next->prev = new;
 	tmp->next = new;
